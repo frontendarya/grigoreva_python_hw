ceb5549ceb25190255e96dc3b1620aa1ad5a1701
grigorevadd <d.grigoreva@i-teco.ru>
2025-03-20 23:47:18 +0300

    Дз 2 сделано 3/3

---
+++ hw_2/2.1.py
@@ -0,0 +1,3 @@
+import hw_2.LatexConverter as LatexConverter
+
+LatexConverter.table_to_latex([['col 1', 'col 2', 'col 3'], [1, 2, 3], [4, 5, 6]])
---
+++ hw_2/2.2.py
@@ -0,0 +1,14 @@
+from my_latex_cute_lib import main
+import subprocess
+
+def convert_tex_to_pdf(table, tex_file):
+    main.table_to_latex(table, tex_file)
+    main.img_to_latex('malone.png', tex_file)
+    subprocess.run(['pdflatex', tex_file])
+
+table_input = input('2D-array: ')
+filename_input = input('filename: ')
+
+table = eval(table_input)
+convert_tex_to_pdf(table, filename_input)
+
